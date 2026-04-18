73f4f4b3f8d4863c681db57ff8f54d29204cf30c
Na <gaona@dreame.tech>
2026-04-18 19:09:16 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,15 @@ module.exports = {
   apps: [
     {
       name: "photo-to-3d",
-      script: "npm",
-      args: "start",
-      instances: "max",
-      exec_mode: "cluster",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      instances: 1,
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
         PORT: 3000
-      }
+      },
+      max_memory_restart: "500M"
     }
   ]
 };
